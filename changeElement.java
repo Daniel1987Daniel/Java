ada0a7d9ea5e9125209bd5032a89c94415d364c7
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-04-26 23:55:48 +0200

    Add files via upload

---
+++ changeElement.java
@@ -0,0 +1,20 @@
+package ExpressionsAndControlFlow;
+
+public class changeElement {
+    public static void main(String[] args) {
+
+        // - Create an array variable named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`.
+
+        int[] numbers1 = {1, 2, 3, 8, 5, 6};
+
+        // - Change the value of the fourth element (8) to 4
+
+        int[] numbers2 = {1, 2, 3, 4, 5, 6};
+        System.out.println("Fourth element is: " + numbers1[3]);
+
+        // - Print the fourth element
+
+        System.out.println("Fourth element is: " + numbers2[3]);
+    }
+}
+
