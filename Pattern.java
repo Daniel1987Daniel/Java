5cc4b6af06397b609bb1d36229f46e88c8d4f39c
Daniel1987Daniel <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-05-15 21:19:32 +0200

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int i, j;
+        int n = 6;
+        for (i = n; i > 0; i--) {
+            for (j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+/*
+
+output:
+
+******
+*****
+****
+***
+**
+*
+
+*/
